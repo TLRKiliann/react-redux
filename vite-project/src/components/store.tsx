2fd0d854be8dc03c45c25a6b087243a9016badae
TLRKiliann <philogenie@protonmail.com>
2024-05-17 17:17:39 +0200

    some changes

--- vite-project/src/components/store.tsx
+++ vite-project/src/components/store.tsx
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-// Définition des types d'état et d'action
 type StateTypes = {
     counter: number;
 }
@@ -9,17 +8,14 @@ type ActionTypes = {
     type: string;
 }
 
-// État initial du compteur
 const initialCount: StateTypes = { counter: 0 };
 
-// Définition des types d'action
 const actionTypes = {
     INCREMENT: 'increment',
     DECREMENT: 'decrement',
     RESET: 'reset'
 };
 
-// Actions pour incrémenter et décrémenter le compteur
 export const actions = {
     /* increment: () => ({ type: actionTypes.INCREMENT }),
     decrement: () => ({ type: actionTypes.DECREMENT }) */
@@ -28,7 +24,7 @@ export const actions = {
     reset: () => ({ type: 'reset' } as const)
 }
 
-// Réducteur pour gérer les actions et mettre à jour l'état
+// Manage actions & update state
 const reducer = (state: StateTypes = initialCount, action: ActionTypes): StateTypes => {
     switch (action.type) {
         case actionTypes.INCREMENT:
@@ -42,11 +38,10 @@ const reducer = (state: StateTypes = initialCount, action: ActionTypes): StateTy
     }
 }
 
-// Configuration du store avec le réducteur
+// createStore is depreciated
 export const store = configureStore({
     reducer
 });
 
-// Définition des types RootState et AppDispatch à partir du store
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
