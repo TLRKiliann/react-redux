5a5f54f7e9c662ef4d9ff4b43a0dbd6cd9e3ccea
TLRKiliann <philogenie@protonmail.com>
2024-05-17 17:11:14 +0200

    some changes & reset added

--- vite-project/src/components/store.tsx
+++ vite-project/src/components/store.tsx
@@ -1,42 +1,52 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+// Définition des types d'état et d'action
 type StateTypes = {
     counter: number;
 }
 
 type ActionTypes = {
-    type: 'increment';
-    payload: number;
+    type: string;
 }
 
-const initialCount = { counter: 0 };
+// État initial du compteur
+const initialCount: StateTypes = { counter: 0 };
 
+// Définition des types d'action
 const actionTypes = {
     INCREMENT: 'increment',
-    DECREMENT: 'decrement'
+    DECREMENT: 'decrement',
+    RESET: 'reset'
 };
 
+// Actions pour incrémenter et décrémenter le compteur
 export const actions = {
-    increment: () => ({type: actionTypes.INCREMENT}),
-    decrement: () => ({type: actionTypes.DECREMENT})
+    /* increment: () => ({ type: actionTypes.INCREMENT }),
+    decrement: () => ({ type: actionTypes.DECREMENT }) */
+    increment: () => ({ type: 'increment' } as const),
+    decrement: () => ({ type: 'decrement' } as const),
+    reset: () => ({ type: 'reset' } as const)
 }
 
-const reducer = (state: StateTypes = initialCount, action: ActionTypes) => {
+// Réducteur pour gérer les actions et mettre à jour l'état
+const reducer = (state: StateTypes = initialCount, action: ActionTypes): StateTypes => {
     switch (action.type) {
         case actionTypes.INCREMENT:
-            return {...state, counter: state.counter + 1};
+            return { ...state, counter: state.counter + 1 };
         case actionTypes.DECREMENT:
-            return {...state, counter: state.counter - 1};
+            return { ...state, counter: state.counter - 1 };
+        case actionTypes.RESET:
+            return initialCount;
         default:
             return state;
     }
 }
 
+// Configuration du store avec le réducteur
 export const store = configureStore({
     reducer
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+// Définition des types RootState et AppDispatch à partir du store
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
