e09ac39543fd4bd0b69de7df1294f823c623a90d
TLRKiliann <philogenie@protonmail.com>
2024-05-20 21:28:56 +0200

    some changes

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux'
 import App from './App.tsx'
-//import { store } from './components/store'
 import { store } from './app/store'
 import './index.css'
 
